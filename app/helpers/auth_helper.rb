1b95dca2cc2b460634b9c34514cdfa78deca1096
Evalyn Berleant <eberleant@gmail.com>
2021-02-05 16:57:38 -0600

    Debugging

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -8,6 +8,7 @@ module AuthHelper
 
   def set_headers user
     token = issue_token user
+    Rails.logger.info token
     response.set_header('Access-Token', token[:token])
     response.set_header('Access-Token-Expiry', token[:expiry])
   end
