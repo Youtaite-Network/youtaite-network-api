3bf6214f2a9aea0d0a8dfa58c396f2499545eb19
Evalyn Berleant <eberleant@gmail.com>
2021-02-05 17:27:13 -0600

    Fix role creation parameters

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -32,11 +32,14 @@ class RolesController < ApplicationController
     collab = Collab.find_by(yt_id: yt_id)
     if collab.nil? # create new collab
       info = get_collab_info yt_id
-      collab = Collab.create({
+      collab = Collab.new({
         yt_id: yt_id,
         title: info[:title],
         thumbnail: info[:thumbnail],
       })
+      if !collab.save
+        Rails.logger.error collab.errors.full_messages
+      end
     end
     # find each person
     params['people'].each do |person_obj|
@@ -45,23 +48,29 @@ class RolesController < ApplicationController
         misc_id = person_obj['misc_id']
         id_type = person_obj['id_type']
         info = get_person_info(misc_id, id_type)
-        person = Person.create({
+        person = Person.new({
           misc_id: misc_id,
           id_type: id_type,
           name: info[:display_name],
           thumbnail: info[:thumbnail],
         })
+        if !person.save
+          Rails.logger.error person.errors.full_messages
+        end
       else
         person = Person.find(person_obj['id'])
       end
       # add roles
-      person_obj['roles'].each do |role|
-        Role.create({
-          collab_id: collab.yt_id,
+      person_obj['roles'].each do |role_name|
+        role = Role.new({
+          collab_id: collab.id,
           person_id: person.id,
           user_id: current_user.id,
-          role: role,
+          role: role_name,
         })
+        if !role.save
+          Rails.logger.error role.errors.full_messages
+        end
       end
     end
     set_headers current_user
