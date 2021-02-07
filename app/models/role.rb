f352f0cc34e750f23a445221b947f9d9bc4a5fb8
Evalyn Berleant <eberleant@gmail.com>
2021-02-07 02:12:23 -0600

    Add script to roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
-  enum role: [:video, :art, :mix, :vocal, :guide, :translate, :organize, :lyrics, :misc]
+  enum role: [:video, :art, :mix, :vocal, :guide, :translate, :organize, :lyrics, :script, :misc]
   belongs_to :collab
   belongs_to :person
   belongs_to :user
