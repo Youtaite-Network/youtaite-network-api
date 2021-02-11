403db06eac3102ed70d2d912157c708473073502
Evalyn Berleant <eberleant@gmail.com>
2021-02-11 04:13:02 -0600

    Add more id_type options

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ApplicationRecord
-  enum id_type: [:yt, :tw, :no_link, :other, :yt_link, :ig]
+  enum id_type: [:yt, :tw, :no_link, :other, :yt_link, :ig, :fb, :soundcloud, :tumblr]
   has_many :roles
   has_many :collabs, through: :roles
   has_many :collabs
