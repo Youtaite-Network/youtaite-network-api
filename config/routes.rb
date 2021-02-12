40b1a3dbb8bee952d69a31ab6f2509fc7fd6db2d
Evalyn Berleant <eberleant@gmail.com>
2021-02-11 21:27:30 -0600

    Add destroy to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/edges', to: 'collabs#edges'
   get '/collabs/info/:yt_id', to: 'collabs#info'
   get '/collabs/new_random', to: 'collabs#new_random'
+  delete '/collabs', to: 'collabs#destroy'
   get '/people/info/:yt_id', to: 'people#info'
   get 'people/info_from_url/*url', to: 'people#info_from_url'
   post '/googlesignin', to: 'users#signin'
