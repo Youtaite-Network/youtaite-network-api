a0be3fc231e4a42f8658cc1572ab4b0c2e97f59b
Evalyn Berleant <eberleant@gmail.com>
2021-02-16 12:00:35 -0600

    Add people_edges to /edges return json

--- app/models/collab.rb
+++ app/models/collab.rb
@@ -8,20 +8,45 @@ class Collab < ApplicationRecord
 
   def self.edges
     edges = []
+    people_edges = []
     Role.all.group_by(&:person_id).each do |person_id, roles|
       roles.map{|role| role.collab_id}.uniq.combination(2).to_a.each do |first, second|
-        edges += [Set[first, second]]
+        min = [first, second].min
+        max = [first, second].max
+        edges += [[min, max]]
+        people_edges.push({
+          person: person_id,
+          edge: {source: min, target: max},
+        })
       end
     end
+    # create {freq: [edges]} hash
     edges_uniq = edges.uniq
-    edge_by_freq = edges_uniq.group_by do |pair|
+    freq_to_edges = edges_uniq.group_by do |pair|
       edges.count(pair)
-    end.map do |strength, edges_with_strength|
-      [strength, edges_with_strength.map{ |e| 
+    end.map do |freq, edges_with_freq|
+      [freq, edges_with_freq.map{ |e| 
         e_array = e.to_a
         {source: e_array[0], target: e_array[1]} 
       }]
     end.to_h
+    # create {edge: freq} hash
+    edge_to_freq = {}
+    freq_to_edges.each do |freq, edges|
+      edges.each do |edge|
+        edge_to_freq[edge] = {i: -1, l: freq}
+      end
+    end
+    # for each elt in people_edges, add i (index) and l (length/freq)
+    people_edges.map! do |pe|
+      freq = edge_to_freq[pe[:edge]]
+      freq[:i] += 1
+      pe.merge(freq)
+    end
+    return {
+      freq_to_edges: freq_to_edges,
+      people_edges: people_edges,
+    }
   end
 
   private
