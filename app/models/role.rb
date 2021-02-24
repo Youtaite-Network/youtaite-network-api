db26919de04e3e7e175b6c3648ad762d562408df
Evalyn Berleant <eberleant@gmail.com>
2021-02-23 22:45:40 -0600

    Remove belongs_to :user from role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,6 @@ class Role < ApplicationRecord
   enum role: [:video, :art, :mix, :vocal, :guide, :translate, :organize, :lyrics, :script, :misc, :arrange, :instrumental, :compose]
   belongs_to :collab
   belongs_to :person
-  belongs_to :user
   validates :collab_id, uniqueness: { scope: [:person_id, :role] }
   audited
 end
